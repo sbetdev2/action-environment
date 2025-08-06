b81efab3e515172b129cb63395e8ff3b6b6e181c
Raphael Jackson <sportbetdev2@gmail.com>
2025-08-06 15:39:44 +0300

    Stage1

--- src/main.js
+++ src/main.js
@@ -1,19 +1,44 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
 
+const isProductionHostName = (hostName) => {
+  [
+    "Prod1",
+    "Prod2",
+    "Prod3",
+    "Prod4",
+    "Broker1",
+    "Broker2"
+  ].includes(hostName);
+}
+
 /**
  * The main function for the action.
  * @returns {Promise<void>} Resolves when the action is complete.
  */
+
 export async function run() {
   try {
-    // The `who-to-greet` input is defined in action metadata file
-    const whoToGreet = core.getInput('who-to-greet', { required: true })
-    core.info(`Hello, ${whoToGreet}!`)
+    // Access secrets from environment variables
+    const mySecret = process.env.MY_SECRET
+    const anotherSecret = process.env.ANOTHER_SECRET
+
+    const gitRef = github.context.ref
+    core.info(`GitHub Ref: ${gitRef}`);
+
+    const hosts = core.getInput('hosts', { required: true })
+    core.info(`Hosts file: ${hosts}!`);
+
+
+    const targetHosts = core.getInput('target-hosts', { required: true })
+    core.info(`Target hosts: ${targetHosts}!`)
+
 
     // Get the current time and set as an output
-    const time = new Date().toTimeString()
-    core.setOutput('time', time)
+    const matrix = hosts.find(h=> targetHosts.includes(h.hostName));
+
+    core.info(`matrix: ${matrix}!`)
+    core.setOutput('matrix', matrix)
 
     // Output the payload for debugging
     core.info(
